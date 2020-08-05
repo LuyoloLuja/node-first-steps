92827928f996ae77972018f1232caa086a86fb36
Luyolo <luyolom19@gmail.com>
2020-08-05 11:58:19 +0200

    added figlet style

--- greet_xola.js
+++ greet_xola.js
@@ -4,3 +4,16 @@ const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage);
+
+// using figlet
+var figlet = require('figlet');
+ 
+figlet(greet('Xola'), function(err, data) {
+
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
